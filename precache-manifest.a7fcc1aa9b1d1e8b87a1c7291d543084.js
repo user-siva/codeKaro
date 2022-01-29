d89d115861122b62ebd23dfcfde8535d7c2ad69d
user-siva <sivag0503@gmail.com>
2022-01-29 14:37:55 +0530

    Updates

---
+++ precache-manifest.a7fcc1aa9b1d1e8b87a1c7291d543084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262738eac2d9d53004beb3e5b6860d7c",
+    "url": "/codeKaro/index.html"
+  },
+  {
+    "revision": "bdbc5c03febf0905cb02",
+    "url": "/codeKaro/static/css/2.16d56f53.chunk.css"
+  },
+  {
+    "revision": "35403bbda23d4c00d0d1",
+    "url": "/codeKaro/static/css/main.d156b3d8.chunk.css"
+  },
+  {
+    "revision": "bdbc5c03febf0905cb02",
+    "url": "/codeKaro/static/js/2.c8b61973.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/codeKaro/static/js/2.c8b61973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35403bbda23d4c00d0d1",
+    "url": "/codeKaro/static/js/main.20bb862d.chunk.js"
+  },
+  {
+    "revision": "4aff461b151b22ff9604",
+    "url": "/codeKaro/static/js/runtime-main.6f5c9ec7.js"
+  }
+]);
